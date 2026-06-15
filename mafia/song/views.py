12085cb381d4c924b43dbec634b056656dc4df0c
artin231 <leiartrezjah@gmail.com>
2026-06-16 01:58:36 +0330

    i added the head partial

--- mafia/song/views.py
+++ mafia/song/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.views.generic.base import TemplateView
+from .models import Song
 # Create your views here.
-class ListSong(ListView):
+class ListSongView(ListView):
     template_name = 'song/index.html'
+    model = Song
+    context_object_name = 'songs'
+
+class HeaderView(TemplateView):
+    template_name = 'partial/header.html'
