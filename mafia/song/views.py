1d63eed07ace7018efb9576775fcf80d24fe9b28
artin231 <leiartrezjah@gmail.com>
2026-06-18 00:24:22 +0330

    i maked the form and added the audio

--- mafia/song/views.py
+++ mafia/song/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.views import View
 from django.views.generic.base import TemplateView
 from .models import Song
+from django.http import Http404
 # Create your views here.
-class ListSongView(ListView):
-    template_name = 'song/index.html'
-    model = Song
-    context_object_name = 'songs'
+class ListSongView(View):
+    def get(self,request):
+        all_song = Song.objects.all()
+        if all_song is not None:
+            return render(request,'song/index.html',{'songs':all_song,'song':all_song.first()})
+        else:
+            return render(request,'song/index.html',{'songs':[],'song':None})
+    def post(self,request):
+        try:
+            song = request.POST.get('song')
+            find_song = Song.objects.get(name=song)
+            return render(request,'song/index.html',{'songs':Song.objects.all(),'song':find_song[0]})
+        except:
+            raise Http404('song Not found')
 
 class HeaderView(TemplateView):
     template_name = 'partial/header.html'
