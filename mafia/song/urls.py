a88fdcc985ebf3cd09fd595a0afcce97aff844f2
artin231 <leiartrezjah@gmail.com>
2026-06-26 10:18:07 +0330

    i made a new box for the timer and i fixed a bug

--- mafia/song/urls.py
+++ mafia/song/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.ListSongView.as_view(), name='home')
+    path('',views.ListSongView.as_view(), name='home'),
+    path('succses/',views.succes,name='succes')
 ]
--- mafia/song/views.py
+++ mafia/song/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views.generic import ListView
 from django.views import View
 from django.views.generic.base import TemplateView
 from .models import Song
+from django.urls import reverse
 from django.http import Http404
 # Create your views here.
 class ListSongView(View):
@@ -15,8 +16,10 @@ class ListSongView(View):
     def post(self,request):
             song = request.POST.get('song')
             find_song = Song.objects.get(name=song)
-            return render(request,'song/index.html',{'songs':Song.objects.all(),'song':find_song})
+            return redirect(reverse('succes'))
    
+def succes(request):
+    return redirect(reverse('home'))
 
 class HeaderView(TemplateView):
     template_name = 'partial/header.html'
