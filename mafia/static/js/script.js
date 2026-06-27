55eac62e6908e8c251f4a5cb5171ab9c58f616b6
artin231 <leiartrezjah@gmail.com>
2026-06-27 10:58:19 +0330

    i am making the timer and now it starts and its correct but when i stop it and again i run it it goes faster. i added (;) in the end of js lines.

--- mafia/static/js/script.js
+++ mafia/static/js/script.js
@@ -1,28 +1,54 @@
-let play = document.querySelector('.play')
+let play = document.querySelector('.play');
 let played = false;
-let audio = document.querySelector('.player')
+let audio = document.querySelector('.player');
 play.addEventListener('mousedown', async () => {
     select_song = document.querySelector('#select_song');
     console.log('/succses?id=' + select_song.value);
-    
+
     res = await fetch('/succses?id=' + select_song.value);
     data = await res.json();
 
     if (!played) {
-        play.innerHTML = 'pause'
+        play.innerHTML = 'pause';
         console.log(audio);
-        played = true
+        played = true;
         if (data) {
             console.log(data);
-            
+
             audio.src = data.src;
         }
-        audio.play()
+        audio.play();
     }
     else {
-        played = false
-        audio.pause()
-        play.innerHTML = 'play'
+        played = false;
+        audio.pause();
+        play.innerHTML = 'play';
     }
 
 })
+let start = false;
+let timer = () => {
+    let num_timer = document.querySelector('#num');
+    let btn = document.querySelector('#btn_ps');
+    let timer_start = () => {
+        num = Number(num_timer.innerHTML);
+        num -= num_m;
+        num_timer.innerHTML = num;
+    }
+    num_m = 1;
+    var intreval;
+    if (!start) {
+        start = true;
+        btn.innerHTML = 'stop';
+        intreval = setInterval(timer_start, 1000);
+
+    }
+    else {
+        clearInterval(intreval);
+        num_timer.innerHTML = 60;
+        btn.innerHTML = 'start';
+        start = false;
+        num_m = 0;
+    }
+
+}
